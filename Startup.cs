f57688e41087b58e4f06367396a6f0365e9602b5
autodidaddict <alothien@gmail.com>
2016-11-26 13:45:03 -0500

    Adding hello world middleware

---
+++ Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+
+namespace webapp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Run(async (context) =>
+           {
+               await context.Response.WriteAsync("Hello, world!");
+           });
+        }
+    }
+}
