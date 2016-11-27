0bda561c45518908bd5938c6eceb73e3c4f3c7cf
autodidaddict <alothien@gmail.com>
2016-11-27 09:58:56 -0500

    Added a model

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ Models/StockQuote.cs
@@ -0,0 +1,9 @@
+﻿
+namespace webapp.Models
+{
+    public class StockQuote
+    {
+        public string Symbol { get; set; }
+        public int Price { get; set; }
+    }
+}
